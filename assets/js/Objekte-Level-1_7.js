b06c8d3b1632d8a38a7347ad876a7a60b61ce465
Farhad <149811241+Far1had@users.noreply.github.com>
2024-01-02 17:26:21 +0100

    3 commit

---
+++ assets/js/Objekte-Level-1_7.js
@@ -0,0 +1,48 @@
+let myMusic = [
+    {
+      artist: "The Beatles",
+      title: "Abbey Road",
+      medium: "LP"
+    },
+    {
+      artist: "Pink Floyd",
+      title: "Dark Side of the Moon",
+      medium: "CD"
+    },
+    {
+      artist: "Led Zeppelin",
+      title: "Led Zeppelin IV",
+      medium: "LP"
+    },
+    {
+      artist: "Metallica",
+      title: "Ride the Lightning",
+      medium: "CD"
+    }
+  ];
+
+  myMusic.forEach(function(item) {
+    // Alle "artist"
+    document.getElementById("artistsOutput").innerHTML += item.artist + "<br>";
+
+    // Alle "title"
+    document.getElementById("titlesOutput").innerHTML += item.title + "<br>";
+
+    // Alle "medium"
+    document.getElementById("mediumsOutput").innerHTML += item.medium + "<br>";
+  });
+
+myMusic.forEach((all) => {
+    console.log(all.artist);
+  }); 
+
+  myMusic.forEach((all) => {
+    console.log(all.title);
+  }); 
+  myMusic.forEach((all) => {
+    console.log(all.medium);
+  }); 
+
+
+
+
